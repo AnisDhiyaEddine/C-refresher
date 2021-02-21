748043a416212d83ea9f07332b296b6970c90f71
ANISDHIYAEDDINE BOUDIAF <dhiaeboudiaf@gmail.com>
2021-02-21 14:35:34 +0100

    valgrind testing

--- ThreadsChallenge/BOUDIAF_ANISDHIYAEDDINE_12015262.c
+++ ThreadsChallenge/BOUDIAF_ANISDHIYAEDDINE_12015262.c
@@ -14,6 +14,7 @@
 #include <unistd.h>
 
 #define nbrEtudiant 12015262
+
 typedef struct argument_texte_s
 {
   char * texte;
@@ -72,8 +73,7 @@ argument_texte_t creer_argument(char *texte, int taille){
   argument_texte_t initialized;
   initialized.taille_texte = taille;
   initialized.texte = malloc(sizeof(char) * taille);
-  int i;
-  for (i = 0; i < taille;i++){
+  for (int i = 0; i < taille;i++){
     initialized.texte[i] = texte[i];
   }
   return initialized;
@@ -87,9 +87,12 @@ argument_texte_t creer_argument(char *texte, int taille){
  */
 argument_fichier_t *creer_argument_fichier(char *chemin){
   argument_fichier_t * args = malloc(sizeof(argument_fichier_t));
+  args->resultat = '-';
   args->chemin = malloc(sizeof(char) * strlen(chemin));
-  strcpy(args->chemin, chemin);
-  return args;
+  for (int i = 0; i < strlen(chemin);i++){
+    args->chemin[i] = chemin[i];
+  }
+    return args;
 }
 
 /**
@@ -97,7 +100,7 @@ argument_fichier_t *creer_argument_fichier(char *chemin){
    @param args est un pointeur vers la structure argument_fichier_t
  */
 void liberer_argument_fichier(argument_fichier_t *args){
-  args = NULL;
+  args->chemin = NULL;
   free(args);
 }
 
@@ -115,11 +118,11 @@ void * rechercher_texte(void *args){
   argument_texte_t *arg = (void *)args;
   int *found = malloc(sizeof(int));
   *found = 0;
-  if (strstr(arg->texte, "12015262") != NULL)
-  {
-    *found = 1;
-    return (void *)found;
+  if (strlen(arg->texte) >= 9){
+    if (strstr(arg->texte, "12015262") != NULL)
+      *found = 1;
   }
+
   pthread_exit((void *)found);
 }
 
@@ -132,9 +135,10 @@ void * rechercher_texte(void *args){
  */
 pthread_attr_t attributs_recherche_texte()
 {
-  pthread_attr_t *attr = malloc(sizeof(pthread_attr_t));
-  pthread_attr_setstacksize(attr, PTHREAD_STACK_MIN);
-  return *attr;
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+  pthread_attr_setstacksize(&attr, PTHREAD_STACK_MIN);
+  return attr;
 }
 
 /**
@@ -143,9 +147,10 @@ pthread_attr_t attributs_recherche_texte()
  */
 pthread_attr_t attributs_recherche_fichier()
 {
-  pthread_attr_t *attr = malloc(sizeof(pthread_attr_t));
-  pthread_attr_setstacksize(attr,nbrEtudiant);
-  return *attr;
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+  pthread_attr_setstacksize(&attr, nbrEtudiant);
+  return attr;
 }
 
 
@@ -173,16 +178,14 @@ void copie_fichier_chaine(char *chemin, char *chaine, long taille)
 
   while (!feof(fp))
   {
-    chaine[i] = fgetc(fp);
-    i++;
+    chaine[i++] = fgetc(fp);
   }
-  i--;
-  chaine[i] = '\0';
+  chaine[i--] = '\0';
   fclose(fp);
     }
 
     /**
-   Cette fonction decoupe une chaine de caractère en 10 sous chaine égaux en taille sauf le dernier qui est légerment plus petit
+   Cette fonction decoupe une chaine de caractère en 10 sous chaine égaux en taille sauf le dernier qui est légèrement plus petit
    @requires chaine != NULL.
    @requires length > 18.
    @param tab est un tableau de 10 structures argument_texte_t qui vont contenir les differentes parties de la chaine à decouper
@@ -197,13 +200,15 @@ void copie_fichier_chaine(char *chemin, char *chaine, long taille)
       for (i = 0; i < 9; i++)
       {
         for (j = 0; j < block; j++) str[j] = chaine[j + block * i];
+        //strncat(str, chaine + block * i, block);
         tab[i] = creer_argument(str, block);
       }
 
       free(str);
-      str = malloc(sizeof(char)*(length - block * 9));
-      for (j = 0; j < length - block * 9; j++) str[j] = chaine[j - 1 + block * 9];
-       tab[9] = creer_argument(str, (length - block * 9));
+      char *str1 = malloc(sizeof(char) * (length - block * 9));
+      for (j = 0; j < length - block * 9; j++) str1[j] = chaine[j+ block * 9];
+       tab[9] = creer_argument(str1, (length - block * 9));
+      free(str1);
   }
 
   /**
@@ -215,21 +220,23 @@ void copie_fichier_chaine(char *chemin, char *chaine, long taille)
   void *recherche_fichier(void *arg_fichier)
   {
 
+
     argument_fichier_t arg = *(argument_fichier_t *)arg_fichier;
-    char *chaine = malloc(sizeof(char) * 1E6);
-    copie_fichier_chaine(arg.chemin, chaine, taille_fichier(arg.chemin));
+    char *chaine = malloc(sizeof(char) * 1E7);
     argument_texte_t tab[10];
-    decouper(tab, chaine, taille_fichier(arg.chemin));
     int i;
-    pthread_t th[10];
+    pthread_t *th = malloc(sizeof(pthread_t)*10);
     void *ret;
     pthread_attr_t attr = attributs_recherche_texte();
+
+    copie_fichier_chaine(arg.chemin, chaine, taille_fichier(arg.chemin));
+    decouper(tab, chaine, taille_fichier(arg.chemin));
     for (i = 0; i < 10; i++)
     {
       if(pthread_create(&th[i], &attr, rechercher_texte, (void *)&tab[i])){
         perror("error creation threads to operate on file slices");
         exit(EXIT_FAILURE);
-      };
+      } 
     }
 
     for (i = 0; i < 10; i++)
@@ -240,9 +247,16 @@ void copie_fichier_chaine(char *chemin, char *chaine, long taille)
         arg.resultat = '1';
       }
     }
-    ret = NULL;
+
+    
     free(ret);
-    pthread_exit((void *)&arg);
+    free(th);
+    free((void *)chaine);
+    for (i = 0; i < 10;i++){
+      free((void *)tab[i].texte);
+    }
+    pthread_attr_destroy(&attr);
+    pthread_exit(&arg);
     }
 
 
@@ -251,43 +265,46 @@ void copie_fichier_chaine(char *chemin, char *chaine, long taille)
 
     int main(int argc, char **argv)
 {
-
   DIR *dir = opendir(argv[1]);
   int nbrDesFichiers = combien_de_fichiers(dir);
   int i = 0;
+  void *ret;
+
   pthread_t *th = malloc(sizeof(pthread_t) * nbrDesFichiers);
   pthread_attr_t attr = attributs_recherche_fichier();
-  argument_fichier_t *arg = malloc(sizeof(argument_fichier_t));
+  argument_fichier_t argument, *arg = malloc(sizeof(argument_fichier_t));
   struct dirent *contenu;
+
   while ((contenu = readdir(dir)) != NULL)
   {
     if (contenu->d_type == DT_REG)
     {
-
       arg->chemin = contenu->d_name;
       arg->resultat = '-';
-      if(pthread_create(&th[i], &attr, recherche_fichier,creer_argument_fichier(arg->chemin))){
+       if (pthread_create(&th[i], &attr, recherche_fichier,creer_argument_fichier(arg->chemin)))
+      {
         perror("error creation threads to operate on files");
         exit(EXIT_FAILURE);
-      }
+      } 
       i++;
     }
-   }
+  }
 
-  void *ret;
-  argument_fichier_t argument;
-  for (i = 0; i < nbrDesFichiers; i++)
+   for (i = 0; i < nbrDesFichiers; i++)
   {
     pthread_join(th[i], &ret);
     argument = *(argument_fichier_t *)ret;
-    if (argument.resultat == '1')
+    if (argument.resultat == '1'){
       printf("le fichier %s contient mon numéro d'étudiant \n", argument.chemin);
+    }
+  
   }
-  ret = NULL;
-  free(ret);
-
+ 
+    free(th);
+    liberer_argument_fichier(arg);
   return EXIT_SUCCESS;
 }
 
-
+// gcc -Wall BOUDIAF_ANISDHIYAEDDINE_12015262.c -o main -lpthread
+// valgrind --max-stackframe=12000000 --track-origins=yes ./main ./
 
